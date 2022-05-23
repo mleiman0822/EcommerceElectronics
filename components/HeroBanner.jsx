44e236c8247cb7e6ef3717adfb74948a91f607db
Matthew T Leiman <matthewtleiman@gmail.com>
2022-05-23 13:55:58 -0600

    Init commit

---
+++ components/HeroBanner.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import Link from 'next/link';
+
+// import { urlFor } from '../lib/client';
+
+const HeroBanner = () => {
+  return (
+    <div className="hero-banner-container">
+      <div>
+        <p className="beats-solo">SMALL TEXT</p>
+        <h3>MID TEXT</h3>
+        <h1>LARGE TEXT</h1>
+        <img src="" alt="headphones" className="hero-banner-image" />
+
+        <div>
+          <Link href={`/product/Id`}>
+            <button type="button">BUTTON TEXT</button>
+          </Link>
+          <div className="desc">
+            <h5>Description</h5>
+            <p>DESC</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default HeroBanner
